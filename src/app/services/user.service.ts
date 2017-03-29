e88e35e2bae54e92a465db356e0698458778a3e3
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-03-29 23:36:02 +0200

    Adding users service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {Http} from "@angular/http";
+import {environment} from "../../environments/environment";
+import {User} from "../user";
+
+@Injectable()
+export class UserService {
+
+    private backendUrl: string = `${environment.backendUsersUrl}`;
+    private usersUrl = this.backendUrl + '/users/';
+
+    constructor(private http: Http) {}
+
+    public getClient(user_id: string): Promise<User> {
+        return this.http.get(`${this.usersUrl}/${user_id}`).map(response => response.json()).toPromise();
+    }
+
+}
