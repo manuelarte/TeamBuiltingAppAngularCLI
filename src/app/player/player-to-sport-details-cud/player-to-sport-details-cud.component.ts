98a843cf885f9640100ba792fecb488e9f0c6189
Manuel Doncel Martos <manuel.doncel.martos@powerhouse.nl>
2017-04-07 17:53:24 +0200

    Starting the form for sport position

--- src/app/player/player-to-sport-details-cud/player-to-sport-details-cud.component.ts
+++ src/app/player/player-to-sport-details-cud/player-to-sport-details-cud.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {TeamSport, TeamSportPosition} from "../../team-sports";
+import {TeamSportService} from "../../services/sports-service";
 
 @Component({
   selector: 'app-player-to-sport-details-cud',
   templateUrl: './player-to-sport-details-cud.component.html',
-  styleUrls: ['./player-to-sport-details-cud.component.scss']
+  styleUrls: ['./player-to-sport-details-cud.component.scss'],
+  providers: [TeamSportService]
 })
 export class PlayerToSportDetailsCudComponent implements OnInit {
 
-  constructor() { }
+  sports: TeamSport[];
+  isBusyLoadingSports: boolean = true;
+
+  constructor(private teamSportService: TeamSportService) { }
 
   ngOnInit() {
+      this.teamSportService.getTeamSportsAvailable().then(sports => {
+          this.isBusyLoadingSports = false;
+          this.sports = sports;
+      }).catch(error => {
+          this.isBusyLoadingSports = false;
+      })
+  }
+
+  isErrorLoadingSports(): boolean {
+      return this.isBusyLoadingSports == false && this.sports == null;
   }
 
 }
