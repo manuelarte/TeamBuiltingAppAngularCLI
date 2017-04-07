513236801a5d497d6b74bfa9c3f9a80b9eb8b505
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-04-07 18:39:30 +0200

    Improving Player to sport position

---
+++ src/app/profile/player-to-sport-details-modal/player-to-sport-details-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-to-sport-details-modal',
+  templateUrl: './player-to-sport-details-modal.component.html',
+  styleUrls: ['./player-to-sport-details-modal.component.scss']
+})
+export class PlayerToSportDetailsModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
