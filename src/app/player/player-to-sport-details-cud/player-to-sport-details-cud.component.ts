7e7afbf9707844ad459b4b6f2f072258fcafd35a
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-04-08 22:18:33 +0200

    Improving Player to sport position

--- src/app/player/player-to-sport-details-cud/player-to-sport-details-cud.component.ts
+++ src/app/player/player-to-sport-details-cud/player-to-sport-details-cud.component.ts
@@ -4,6 +4,7 @@ import {TeamSportService} from "../../services/sports-service";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {PlayerToTeamSportDetails} from "../../player-to-team-sport-details";
 import {Player} from "../../player";
+import {Team} from "../../team";
 
 @Component({
   selector: 'app-player-to-sport-details-cud',
@@ -13,55 +14,69 @@ import {Player} from "../../player";
 })
 export class PlayerToSportDetailsCudComponent implements OnInit {
 
-  @Input() player: Player;
-  @Input() model: PlayerToTeamSportDetails = new PlayerToTeamSportDetails();
-  sports: TeamSport[];
-  isBusyLoadingSports: boolean = true;
-  @Output() entrySaved: EventEmitter<PlayerToTeamSportDetails> = new EventEmitter<PlayerToTeamSportDetails>();
+    @Input() player: Player;
+    @Input() model: PlayerToTeamSportDetails = new PlayerToTeamSportDetails();
+    sports: TeamSport[];
+    isBusyLoadingSports: boolean = true;
+    @Output() entrySaved: EventEmitter<PlayerToTeamSportDetails> = new EventEmitter<PlayerToTeamSportDetails>();
 
-  sportDetailsForm: FormGroup = new FormGroup({
+    sportDetailsForm: FormGroup = new FormGroup({
         sport: new FormControl('', Validators.required),
         mainPosition: new FormControl('', Validators.required),
-        otherPositions: new FormControl('', ),
+        otherPositions: new FormControl('',),
         bio: new FormControl('', Validators.maxLength(500)),
-  });
-  @Output() form: EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
+    });
+    @Output() form: EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
 
 
-  constructor(private teamSportService: TeamSportService) { }
+    constructor(private teamSportService: TeamSportService) {
+    }
 
-  ngOnInit() {
-      this.model.playerId = this.player.id;
-      this.teamSportService.getTeamSportsAvailable().then(sports => {
-          this.isBusyLoadingSports = false;
-          this.sports = sports;
-      }).catch(error => {
-          this.isBusyLoadingSports = false;
-      })
-  }
+    ngOnInit() {
+        this.model.playerId = this.player.id;
+        this.teamSportService.getTeamSportsAvailable().then(sports => {
+            this.isBusyLoadingSports = false;
+            this.sports = sports;
+        }).catch(error => {
+            this.isBusyLoadingSports = false;
+        })
+    }
 
-  isErrorLoadingSports(): boolean {
-      return this.isBusyLoadingSports == false && this.sports == null;
-  }
+    isErrorLoadingSports(): boolean {
+        return this.isBusyLoadingSports == false && this.sports == null;
+    }
 
-  emitForm(): void {
-    this.form.emit(this.sportDetailsForm);
-  }
+    emitForm(): void {
+        this.form.emit(this.sportDetailsForm);
+    }
 
-  getPositions(sportName: string): TeamSportPosition[] {
-      if (this.sports && this.sports.filter(sport => sport.name == sportName).length > 0) {
-        return this.sports.filter(sport => sport.name == sportName)[0].sportPositions;
-      } else {
-          return [];
-      }
-  }
+    filterTeamSportBySportName(sportName: string): TeamSport[] {
+        return this.sports.filter(sport => sport.name == sportName);
+    }
 
-  getSparePositions(sportName: string): TeamSportPosition[] {
-      if (this.sports) {
-          return this.getPositions(sportName).filter(position => position.abbreviation != this.model.mainPosition);
-      } else {
-          return [];
-      }
-  }
+    getPositions(sportName: string): TeamSportPosition[] {
+        if (this.sports && this.filterTeamSportBySportName(sportName).length == 1) {
+            return this.filterTeamSportBySportName(sportName)[0].sportPositions;
+        } else {
+            return [];
+        }
+    }
+
+    getSparePositions(sportName: string): TeamSportPosition[] {
+        if (this.sports) {
+            return this.getPositions(sportName).filter(position => position.abbreviation != this.model.mainPosition);
+        } else {
+            return [];
+        }
+    }
+
+    isShowSportField(): boolean {
+        if (this.sportDetailsForm.valid && this.filterTeamSportBySportName(this.model.sport).length == 1) {
+            return this.filterTeamSportBySportName(this.model.sport)[0]
+                    .sportPositions.filter(sportPosition => sportPosition.abbreviation == this.model.mainPosition).length == 1;
+        } else {
+            return false;
+        }
+    }
 
 }
