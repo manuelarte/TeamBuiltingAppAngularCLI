094ca845f0cfd1f677a241e505081ab1fda7134f
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-04-21 23:15:21 +0200

    Improving

---
+++ src/app/profile-sidenav/profile-sidenav.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Auth} from "../services/auth-service";
+import {RouterUtilsService} from "../services/router-utils.service";
+
+@Component({
+  selector: 'app-profile-sidenav',
+  templateUrl: './profile-sidenav.component.html',
+  styleUrls: ['./profile-sidenav.component.scss'],
+  providers: [Auth, RouterUtilsService]
+})
+export class ProfileSidenavComponent implements OnInit {
+
+  constructor(private auth: Auth, private routerUtilsService: RouterUtilsService) { }
+
+  ngOnInit() {
+  }
+
+  getUserProfile(): any {
+    return this.auth.userProfile;
+  }
+
+}
