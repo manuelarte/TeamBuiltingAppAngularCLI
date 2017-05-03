dd15cde6cb5c1f036c828e88d5e66a8395e4d4c6
Manuel Doncel Martos <manuel.doncel.martos@powerhouse.nl>
2017-05-03 17:44:59 +0200

    Updating google maps

--- src/app/services/player-history-utils.service.ts
+++ src/app/services/player-history-utils.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {PlayerToTeam} from "../player-to-team";
+import {PlayerToTeam} from '../player-to-team';
 
 @Injectable()
 export class PlayerHistoryUtilsService {
@@ -8,18 +8,18 @@ export class PlayerHistoryUtilsService {
 
   public getPlayerHistoryPerTeam(playerHistory: PlayerToTeam[]): {[teamId: string]: PlayerToTeam[]} {
       if (!playerHistory) {
-          throw "Input cannot be null";
+          throw new Error('Input cannot be null');
       }
-      let toReturn: {[teamId: string]: PlayerToTeam[]} = {};
+      const toReturn: {[teamId: string]: PlayerToTeam[]} = {};
       playerHistory.forEach(entry => {
           toReturn[entry.teamId] != null ? toReturn[entry.teamId].push(entry) : toReturn[entry.teamId] = [entry];
       });
       return toReturn;
   }
-  
+
   public sortByFromDate(a: PlayerToTeam, b: PlayerToTeam): number {
       if (!a || !b) {
-          throw "Input cannot be null";
+          throw new Error('Input cannot be null');
       }
       return a.fromDate.getTime() - b.fromDate.getTime();
   }
