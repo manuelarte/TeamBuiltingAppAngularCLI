179b9372853aba30538e324d885048cc2a45788f
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-05-14 15:07:24 +0200

    Improving

--- src/app/player/player-in-team/player-in-team.component.ts
+++ src/app/player/player-in-team/player-in-team.component.ts
@@ -1,15 +1,18 @@
-import {Input, Component, OnInit} from "@angular/core";
+import {Input, Component, OnInit} from '@angular/core';
 
-import {Season} from "../../services/season-utils.service";
-import {Player} from "../../player";
-import {Team} from "../../team";
-import {RouterUtilsService} from "../../services/router-utils.service";
-import {MdDialog} from "@angular/material";
-import {GiveRewardComponent} from "../give-reward/give-reward.component";
-import {TeamService} from "app/services/team.service";
-import {PlayerService} from "app/services/player.service";
-import {PlayerToTeam} from "../../player-to-team";
-import {DatesService} from "../../services/dates-service";
+import {Season} from '../../services/season-utils.service';
+import {Player} from '../../player';
+import {Team} from '../../team';
+import {RouterUtilsService} from '../../services/router-utils.service';
+import {MdDialog} from '@angular/material';
+import {GiveRewardComponent} from '../give-reward/give-reward.component';
+import {TeamService} from 'app/services/team.service';
+import {PlayerService} from 'app/services/player.service';
+import {PlayerToTeam} from '../../player-to-team';
+import {DatesService} from '../../services/dates-service';
+import {UserRightsService} from "../../services/user-rights.service";
+import {UserDataService} from "../../services/user-data.service";
+import {UserData} from "../../user-data";
 /**
     * @author Manuel
     * @since 05/11/2016
@@ -18,7 +21,7 @@ import {DatesService} from "../../services/dates-service";
   selector: 'app-player-in-team',
   templateUrl: 'player-in-team.component.html',
   styleUrls: ['player-in-team.component.scss', 'player-card.scss'],
-  providers: [TeamService, PlayerService, DatesService, RouterUtilsService]
+  providers: [TeamService, PlayerService, DatesService, UserDataService, UserRightsService, RouterUtilsService]
 })
 export class PlayerInTeamComponent implements OnInit {
 
@@ -33,8 +36,14 @@ export class PlayerInTeamComponent implements OnInit {
   @Input() playerToTeam: PlayerToTeam;
   @Input() season: Season;
 
+  userData: UserData;
+  loadingUserData = false;
+  errorLoadingUserData = false;
+
   constructor(private teamService: TeamService, private playerService: PlayerService,
-              private datesService: DatesService, public routerUtilsService: RouterUtilsService, public dialog: MdDialog) {}
+              private datesService: DatesService,
+              private userRightsService: UserRightsService, private userDataService: UserDataService,
+              public routerUtilsService: RouterUtilsService, public dialog: MdDialog) {}
 
 
   ngOnInit(): void {
@@ -55,8 +64,16 @@ export class PlayerInTeamComponent implements OnInit {
         }).catch(error => {
             this.playerLoadingFlag = false;
             this.errorPlayerLoadingFlag = true;
-        })
+        });
     }
+    this.loadingUserData = true;
+    this.userDataService.getUserPlayerData().then(userData => {
+      this.userData = userData;
+      this.loadingUserData = false;
+    }).catch(error => {
+      this.loadingUserData = false;
+      this.errorLoadingUserData = true;
+    });
   }
 
   getPicture() {
@@ -73,26 +90,31 @@ export class PlayerInTeamComponent implements OnInit {
   }
 
   isBusy(): boolean {
-    return this.teamLoadingFlag || this.playerLoadingFlag;
+    return this.teamLoadingFlag || this.playerLoadingFlag || this.loadingUserData;
   }
 
   timeInTeam(): string {
     const endDate: Date = this.playerToTeam.toDate ? new Date(this.playerToTeam.toDate) : new Date();
-    const timeDiff: {years: number, months: number, days: number} = this.datesService.getTimeBetweenTwoDates(endDate, new Date(this.playerToTeam.fromDate));
-    let years: number = Math.floor(timeDiff.months/12);
-    let spareMonths: number = Math.abs(((timeDiff.years*12)-timeDiff.months));
-    let spareDays: number = Math.abs((timeDiff.months*31) - timeDiff.days);
+    const timeDiff: {years: number, months: number, days: number} =
+        this.datesService.getTimeBetweenTwoDates(endDate, new Date(this.playerToTeam.fromDate));
+    const years: number = Math.floor(timeDiff.months / 12);
+    const spareMonths: number = Math.abs(((timeDiff.years * 12) - timeDiff.months));
+    const spareDays: number = Math.abs((timeDiff.months * 31) - timeDiff.days);
     let message = '';
     if (years > 0) {
-        message += years + " years ";
+        message += years + ' years ';
     }
     if (spareMonths > 0) {
-        message += spareMonths + " months "
+        message += spareMonths + ' months ';
     }
     if (spareDays > 0) {
-        message += spareDays + " days";
+        message += spareDays + ' days';
     }
     return message;
+  }
+
+  canUserGiveReward(): boolean {
+    return this.userRightsService.userCanGiveReward(this.userData, this.playerToTeam.playerId);
   }
 
 }
--- src/app/services/user-rights.service.ts
+++ src/app/services/user-rights.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Auth} from "./auth-service";
-import {UserData} from "../user-data";
+import {Auth} from './auth-service';
+import {UserData} from '../user-data';
 
 @Injectable()
 export class UserRightsService {
@@ -8,16 +8,16 @@ export class UserRightsService {
   constructor(private auth: Auth) { }
 
   public userCanGiveReward(userData: UserData, playerId: number): boolean {
-    return this.auth.userProfile ? userData.playerId != playerId : false;
+    return this.auth.userProfile ? userData.playerId !== playerId : false;
   }
 
-    public userCanWriteComment(userData: UserData, playerId: number): boolean {
-        return this.auth.userProfile ? userData.playerId != playerId : false;
-    }
+  public userCanWriteComment(userData: UserData, playerId: number): boolean {
+    return this.auth.userProfile ? userData.playerId !== playerId : false;
+  }
 
   public userCanEdit(userData: UserData, playerId: number): boolean {
     if (!playerId || !userData) {
-     throw new Error("Inputs cannot be null");
+     throw new Error('Inputs cannot be null');
     }
     return this.auth.userProfile ? userData.playerId === playerId : false;
   }
