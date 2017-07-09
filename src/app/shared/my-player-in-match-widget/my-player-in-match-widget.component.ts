01608b54be2e69729d7ce50880cbcbbf0bc686a0
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-07-09 17:01:51 +0200

    Skeleton add match

--- src/app/shared/my-player-in-match-widget/my-player-in-match-widget.component.ts
+++ src/app/shared/my-player-in-match-widget/my-player-in-match-widget.component.ts
@@ -1,14 +1,15 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {PlayerInfo} from '../../match/playerInfo';
+import {PlayerInfo, RegisteredPlayerInfo, UnRegisteredPlayerInfo} from '../../match/playerInfo';
 import {FormControl} from '@angular/forms';
 import {Match} from '../../match/match';
 import {UtilsService} from '../../services/utils.service';
+import {PlayerService} from '../../services/player.service';
 
 @Component({
   selector: 'app-my-player-in-match-widget',
   templateUrl: './my-player-in-match-widget.component.html',
   styleUrls: ['./my-player-in-match-widget.component.scss'],
-  providers: [UtilsService]
+  providers: [PlayerService, UtilsService]
 })
 export class MyPlayerInMatchWidgetComponent implements OnInit {
 
@@ -19,14 +20,14 @@ export class MyPlayerInMatchWidgetComponent implements OnInit {
   isBusy = false;
   isErrorLoading = false;
 
-  constructor(private utilsService: UtilsService) { }
+  constructor(private playerService: PlayerService, private utilsService: UtilsService) { }
 
   ngOnInit() {
       if (this.playersAreSelected()) {
           const homePlayersSelected: PlayerInfo[] = this.schema.widget.match.homeTeam.selectedPlayers?this.schema.widget.match.homeTeam.selectedPlayers:[];
           const awayPlayersSelected: PlayerInfo[] = this.schema.widget.match.awayTeam.selectedPlayers?this.schema.widget.match.awayTeam.selectedPlayers:[];
-          homePlayersSelected.forEach(homePlayer => this.playersRepresentation.push(this.convertPlayerInfoToRepresentation(homePlayer)));
-          awayPlayersSelected.forEach(awayPlayer => this.playersRepresentation.push(this.convertPlayerInfoToRepresentation(awayPlayer)));
+          homePlayersSelected.forEach(homePlayer => this.convertPlayerInfoToRepresentation('http://www.devo58.nl/wordpress/wp-content/themes/devo-activello/images/devo58-logo@2x.png', homePlayer));
+          awayPlayersSelected.forEach(awayPlayer => this.convertPlayerInfoToRepresentation('http://doestkeepersclass.nl/wp-content/uploads/2016/03/logo_wv-hedw.png', awayPlayer));
       }
   }
 
@@ -41,16 +42,23 @@ export class MyPlayerInMatchWidgetComponent implements OnInit {
     return this.playersRepresentation;
   }
 
-  private convertPlayerInfoToRepresentation(playerInfo: PlayerInfo): PlayerRepresentation {
-      if (this.utilsService.isRegisteredPlayer(playerInfo)) {
-
-      }
+  private convertPlayerInfoToRepresentation(teamEmblem: string, playerInfo: PlayerInfo): void {
       const representation = new PlayerRepresentation();
       representation.id = playerInfo.id;
-      representation.teamEmblem = 'http://www.devo58.nl/wordpress/wp-content/themes/devo-activello/images/devo58-logo@2x.png';
-      representation.playerImage = 'http://i.cdn.cnn.com/cnn/.e/interactive/2016/ultimate-footballer-v2/intro1.png';
-      representation.name = 'Manuel Doncel Martos';
-      return representation;
+      representation.teamEmblem = teamEmblem;
+      if (this.utilsService.isRegisteredPlayer(playerInfo)) {
+        const registeredPlayerInfo: RegisteredPlayerInfo = <RegisteredPlayerInfo> playerInfo;
+        this.playerService.getPlayer(registeredPlayerInfo.playerId).then(player => {
+          representation.name = player.name;
+          representation.playerImage = player.imageLink;
+          this.playersRepresentation.push(representation);
+        });
+      } else {
+        const unRegisteredPlayerInfo: UnRegisteredPlayerInfo = <UnRegisteredPlayerInfo> playerInfo;
+        representation.playerImage = 'http://i.cdn.cnn.com/cnn/.e/interactive/2016/ultimate-footballer-v2/intro1.png';
+        representation.name = unRegisteredPlayerInfo.name;
+        this.playersRepresentation.push(representation);
+      }
   }
 
 }
