f9d517463dfa404daac4a759ca518c592da413c6
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-11-14 13:52:30 +0100

    Setting up to fix bugs

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http} from "@angular/http";
 import {environment} from "../../environments/environment";
 import {User} from "../user";
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class UserService {
@@ -11,8 +12,8 @@ export class UserService {
 
     constructor(private http: Http) {}
 
-    public getUser(user_id: string): Promise<User> {
-        return this.http.get(`${this.usersUrl}/${user_id}`).map(response => response.json()).toPromise();
+    public getUser$(user_id: string): Observable<User> {
+        return this.http.get(`${this.usersUrl}/${user_id}`).map(response => response.json());
     }
 
 }
